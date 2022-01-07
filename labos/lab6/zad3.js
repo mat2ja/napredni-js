3e47c6d6e1c5aee97f993db0e968d120ed62f4f3
Matija Osrečki <matija.osrecki@gmail.com>
2022-01-07 16:13:15 +0100

    add missing files

--- labos/lab6/zad3.js
+++ labos/lab6/zad3.js
@@ -1,10 +1,43 @@
 const { createServer } = require('http');
 const url = require('url');
+const { readFile } = require('fs');
+const { promisify } = require('util');
+const readFileAsync = promisify(readFile);
 
-createServer((req, res) => {
-  const { pathname } = url.parse(req.url, true);
-  console.log(pathname);
-  res.writeHead(200, { 'Content-Type': 'text/json' });
-  res.write(`Url: ${pathname}`);
-  res.end();
-}).listen(8001);
+createServer(async (req, res) => {
+  const { pathname, query } = url.parse(req.url, true);
+
+  try {
+    switch (pathname) {
+      case '/': {
+        const data = await readFileAsync('./index.html', 'utf8');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data.toString());
+        res.end();
+        break;
+      }
+      case '/z': {
+        const { x, y } = query;
+        const result = Number(x) + Number(y);
+        res.write(result.toString());
+        res.end();
+        break;
+      }
+      case '/o': {
+        const { x, y } = query;
+        const result = Number(x) - Number(y);
+        res.write(result.toString());
+        res.end();
+        break;
+      }
+      default: {
+        throw new Error();
+      }
+    }
+  } catch (err) {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.write('An error occurred');
+    res.end();
+    console.error(err);
+  }
+}).listen(8888);
--- primjeri/P10 - NodeJS/NJP10-15.js
+++ primjeri/P10 - NodeJS/NJP10-15.js
@@ -1,37 +1,33 @@
 const mysql = require('promise-mysql');
 const http = require('http');
 
-mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'njp'
-}).then(connection => {
-
-    console.log("Database is connected ... ");
-    http.createServer(function (request, response) {
-
+mysql
+  .createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'njp',
+  })
+  .then((connection) => {
+    console.log('Database is connected ... ');
+    http
+      .createServer(function (request, response) {
         let query = connection.query('SELECT * FROM countries');
-       // connection.end();
-        query.then(rows =>{
-
-            rows.forEach(function(country){
-
-                console.log(country.name);
-
+        // connection.end();
+        query
+          .then((rows) => {
+            rows.forEach(function (country) {
+              console.log(country.name);
             });
 
-            response.writeHead(200, {'Content-Type': 'application/json'});
+            response.writeHead(200, { 'Content-Type': 'application/json' });
             response.write(JSON.stringify(rows));
             response.end();
-
-        }).catch(err => console.log('Error while performing Query.'));
-
-
-
-    }).listen(8081);
+          })
+          .catch((err) => console.log('Error while performing Query.'));
+      })
+      .listen(8081);
 
     console.log('Server running at http://127.0.0.1:8081/');
-
-}).catch(err => console.log('Error connecting database'));
-
+  })
+  .catch((err) => console.log('Error connecting database'));
